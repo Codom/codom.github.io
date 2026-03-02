7c57fcf236ee0954b368299f5c542188f9a94048
Christopher Odom <christopher.r.odom@gmail.com>
2026-03-02 13:24:26 -0500

    Attempting a 404 redirect to force-fetch the SPA
    I want people to be able to access codom.github.io/resume
    to see the resume page. I tried out hash routing, but
    this requries a /#/resume in order to work.

--- src/main.js
+++ src/main.js
@@ -7,4 +7,12 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router);
+
+// Handle redirect from 404.html
+const redirectPath = sessionStorage.getItem('redirectPath');
+if (redirectPath) {
+  sessionStorage.removeItem('redirectPath');
+  router.replace(redirectPath);
+}
+
 app.mount('#app')
